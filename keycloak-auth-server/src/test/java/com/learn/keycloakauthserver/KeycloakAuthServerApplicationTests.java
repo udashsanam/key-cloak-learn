b6cfdc2dc2b17340b40ce833ceb82fe80573d7ed
udashsanam <udashsanam@gmail.com>
2023-04-08 15:17:23 +0545

    add: auth server base project

---
+++ keycloak-auth-server/src/test/java/com/learn/keycloakauthserver/KeycloakAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.keycloakauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
