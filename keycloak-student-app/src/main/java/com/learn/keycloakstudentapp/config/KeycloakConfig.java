2798d77b572e5f0858e63c864ea4515125458f34
udashsanam <udashsanam@gmail.com>
2024-04-25 10:23:50 +0545

    add:

--- keycloak-student-app/src/main/java/com/learn/keycloakstudentapp/config/KeycloakConfig.java
+++ keycloak-student-app/src/main/java/com/learn/keycloakstudentapp/config/KeycloakConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class KeycloakConfig {
 
+
+
 	@Bean
 	KeycloakConfigResolver keycloakConfigResolver() {
 		return new KeycloakSpringBootConfigResolver();
